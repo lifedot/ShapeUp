fa34c8f20bb0771aff1d2b07b8caa9498a3302b3
Donald Smith <donaldsmith1911@gmail.com>
2021-06-23 14:11:45 -0500

    Shapes

---
+++ app.js
@@ -0,0 +1,33 @@
+
+
+
+
+class Shape {
+    constructor(
+
+    )
+}
+
+class Circle {
+    constructor(
+
+    )
+}
+
+class Triangle {
+    constructor(
+
+    )
+}
+
+class Rectangle {
+    constructor(
+
+    )
+}
+
+class Square {
+    constructor(
+        
+    )
+}
